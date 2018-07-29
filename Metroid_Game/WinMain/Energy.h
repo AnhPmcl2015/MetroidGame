17b4d863fb0f7606541a1f1b36009ae619181565
Nguyen Quang Hai <kid_katou@yahoo.com.vn>
2018-07-29 18:20:06 +0700

    temp commit

---
+++ Metroid_Game/WinMain/Energy.h
@@ -0,0 +1,17 @@
+#pragma once
+#include "Item.h"
+#include "Sprite.h"
+#include "Define.h"
+
+class Energy : public Item
+{
+private:
+	Sprite * energy;
+public:
+	Energy(LPD3DXSPRITE spriteHandler, World* world);
+	~Energy();
+	void InitSprites(LPDIRECT3DDEVICE9 d3ddv, LPDIRECT3DTEXTURE9 texture);
+	void Init(float posX, float posY);
+	void Update(int t);
+	void Render();
+};
